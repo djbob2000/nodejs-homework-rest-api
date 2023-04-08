de727c86cb9be2bba39b6dc0d918eabd42aceb58
Владимир Перетятько <djbob2000@gmail.com>
2023-04-08 13:50:12 +0300

    pass res to next

--- controllers/users/resendVerificationEmail.js
+++ controllers/users/resendVerificationEmail.js
@@ -4,17 +4,17 @@ require("dotenv").config();
 const { BASE_URL } = process.env;
 const sendEmail = require("../../utils/sendEmail");
 
-const resendVerificationEmail = async (req, res) => {
+const resendVerificationEmail = async (req, res, next) => {
   const { email } = req.body;
   if (!email) {
-    return res.status(400).json({ message: "missing required field email" });
+    return next(CreateError(400, "missing required field email"));
   }
   const user = await User.findOne({ email });
   if (!user) {
-    throw CreateError(401, "Email not found");
+    return next(CreateError(401, "Email not found"));
   }
   if (user.verify) {
-    throw CreateError(400, "Verification has already been passed");
+    return next(CreateError(400, "Verification has already been passed"));
   }
   const verifyEmail = {
     to: email,
